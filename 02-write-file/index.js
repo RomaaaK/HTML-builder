df4d15876a38a864cddedecf5722979b6d4ebf64
RomaaaK <zimpsonzya@gmail.com>
2022-05-19 21:38:45 +0300

    feat: implement task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,22 @@
+const { stdin, stdout, exit } = process;
+const fs = require('fs');
+const path = require('path');
+
+stdout.write('Для выхода напиши exit или сочетание клавиш ctr + c \n');
+stdout.write('Введите текст' + '\n' + '> ');
+
+stdin.on('data', (data) => {
+  const text = data.toString().trim();
+  if (text === 'exit') sayExit();
+  fs.appendFile(path.resolve(__dirname, 'userData.txt'), text + '\n', (err) => {
+    if (err) console.log(err);
+    stdout.write('> ');
+  });
+});
+
+function sayExit() {
+  stdout.write('\ngood bye ;) \n');
+  exit();
+}
+
+process.on('SIGINT', sayExit);
