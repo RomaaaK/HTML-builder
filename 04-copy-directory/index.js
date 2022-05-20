ee683619894c7a2594b36b60abb58d31d5f24bf6
RomaaaK <zimpsonzya@gmail.com>
2022-05-20 21:26:20 +0300

    feat: implement 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const path = require('path');
+const fs = require('fs');
 
+const filesDir = path.resolve(__dirname, 'files');
+const nameNewDir = 'files-copy';
+
+try {
+  const files = fs.promises.readdir(filesDir);
+  fs.mkdir(path.resolve(__dirname, nameNewDir), {recursive: true}, (err) => {
+    if (err) throw err;
+  });
+  files.then((data) => {
+    data.forEach((file) => {
+      const input = fs.createReadStream(path.resolve(filesDir, file), 'utf-8');
+      const output = fs.createWriteStream(path.resolve(__dirname, nameNewDir, file));
+      input.pipe(output);
+    });
+  });
+  console.log('copy files done');
+} catch (err) {
+  console.log(err);
+}
